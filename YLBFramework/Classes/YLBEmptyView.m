b02d2ae3a67a7a82883419055479e36d93b0ae4e
yulibo <564885081@qq.com>
2021-09-22 11:44:32 +0800

    修改加载方式

--- YLBFramework/Classes/YLBEmptyView.m
+++ YLBFramework/Classes/YLBEmptyView.m
@@ -17,19 +17,18 @@
     self = [super initWithFrame:frame];
     if (self) {
 //        [self setupSubviews];
-        NSString *imgPath = @"YLBFramework.bundle/icon_grid_emptyView";
-        UIImage *image = [UIImage imageNamed:imgPath
-                              inBundle: [NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil];
-        
-        UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.bounds];
-        _imageView = imageView;
-        imageView.image = image;
-        [self addSubview:imageView];
-
-
+//        [self setupSubviews2];
+        [self setupSubviews3];
     }
     return self;
 }
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    _imageView.frame = self.bounds;
+}
+
 
 - (void)setupSubviews {
     NSBundle *bundle;
@@ -52,10 +51,22 @@
     [self addSubview:imageView];
 }
 
-- (void)layoutSubviews {
-    [super layoutSubviews];
+- (void)setupSubviews2 {
+    NSString *imgPath = @"YLBFramework.bundle/icon_grid_emptyView";
+    UIImage *image = [UIImage imageNamed:imgPath
+                          inBundle: [NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil];
     
-    _imageView.frame = self.bounds;
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.bounds];
+    _imageView = imageView;
+    imageView.image = image;
+    [self addSubview:imageView];
 }
 
+- (void)setupSubviews3 {
+    NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"Frameworks/YLBFramework.framework/YLBFramework.bundle" ofType:nil];
+    NSBundle *bundle = [NSBundle bundleWithPath:bundlePath];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.bounds];
+    imageView.image = [UIImage imageNamed:@"icon_grid_emptyView" inBundle:bundle compatibleWithTraitCollection:nil];
+    [self addSubview:imageView];
+}
 @end
